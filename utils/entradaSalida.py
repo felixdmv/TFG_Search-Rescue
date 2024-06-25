4edc86861e9afc609a519004820da3347ded659c
Félix de Miguel Villalba <fdv1004@alu.ubu.es>
2024-06-25 21:11:51 +0200

    defusedXML instead of xml elementTree

--- utils/entradaSalida.py
+++ utils/entradaSalida.py
@@ -1,4 +1,64 @@
 import json
+from PIL import Image
+import yaml
+import gdown
+from defusedxml import ElementTree as ET
+
+def cargaParametrosConfiguracionYAML(ficheroConfiguracion):
+    try:
+        with open(ficheroConfiguracion, 'r', encoding='utf-8') as archivo_config:
+            configuracion = yaml.safe_load(archivo_config)
+    except FileNotFoundError as error:
+        print(f"Error: {error}")
+        return None
+    return configuracion
+
+def cargaArchivoDrive(url, output):
+    """
+    Downloads a file from the specified URL and saves it to the specified output path.
+
+    Args:
+        url (str): The URL of the file to download.
+        output (str): The path where the downloaded file should be saved.
+
+    Returns:
+        None
+    """
+    gdown.download(url, output, quiet=False)
+
+def cargaParametrosProcesamiento(ficheroParametros):
+    with open(ficheroParametros, 'r') as archivo:
+        parametros = json.load(archivo)
+    return parametros
+
+# Función para obtener información sobre los objetos en una imagen
+def rectangulosEtiquetados(xml_file):
+    rectangulosConHumano = []
+    xml_tree = ET.parse(xml_file)
+    root = xml_tree.getroot()
+    objects = root.findall('object')
+
+    for obj in objects:
+        bndbox = obj.find('bndbox')
+        xmin = int(bndbox.find('xmin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymin = int(bndbox.find('ymin').text)
+        ymax = int(bndbox.find('ymax').text)
+
+        rectangulosConHumano.append((xmin, ymin, xmax, ymax))
+
+    return rectangulosConHumano
+
+def cargaImagen(ficheroImagen):
+    imagen = Image.open(ficheroImagen)
+    return imagen
+
+if __name__ == '__main__':
+    pass
+
+
+'''
+import json
 import xml.etree.ElementTree as ET
 import gdown
 from PIL import Image
@@ -61,3 +121,4 @@ def cargaImagen(ficheroImagen):
 
 if __name__ == '__main__':
     pass
+'''
